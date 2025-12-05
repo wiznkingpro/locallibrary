d33cea03ec501e00a83105fe7e488c87e65d3037
Владислав Плотников <gr436_plval@lab-203-3.tomtit-tomsk.ru>
2025-12-05 10:27:12 +0700

    finish

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
